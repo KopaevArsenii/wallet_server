7a9ecb6f8040f437970c6bba8d4ac5dcac072b3c
Arsenii Kopaev <Senya.Kopaev@mail.ru>
2023-08-31 15:21:31 +0300

    init + made base auth

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import * as argon2 from "argon2"
+import { JwtService } from '@nestjs/jwt';
+import { IUser } from './types';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService
+  ) {}
+
+  async validateUser(email: string, pass: string): Promise<any> {
+    const user = await this.userService.findOne(email);
+    const isPasswordValid = await argon2.verify(user.password, pass)
+    if (user && isPasswordValid) {
+      return user
+    }
+    return new UnauthorizedException("User or password incorrect")
+  }
+
+  async login(user: IUser) {
+    const { id, email } = user;
+    return {
+      id, email, token: this.jwtService.sign({ id, email })
+    }
+  }
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,42 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+import * as argon2 from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private readonly jwtService: JwtService
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const isExist = await this.userRepository.findOne({
+      where: {
+        email: createUserDto.email
+      }
+    });
+    if (isExist) throw new BadRequestException(['This email already exist']);
+
+    const user = await this.userRepository.save({
+      email: createUserDto.email,
+      password: await argon2.hash(createUserDto.password)
+    })
+
+    const token = this.jwtService.sign({ email: createUserDto.email })
+
+    return { token, user };
+  }
+
+  async findOne(email: string): Promise<User | undefined> {
+    return await this.userRepository.findOne({
+      where: {
+        email: email
+      }
+    })
+  }
+
+}
